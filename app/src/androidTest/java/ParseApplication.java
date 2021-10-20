8ee1b9d427ffbb3d05cf683a0137557d0a1cceda
jslvrider <jslvrider@gmail.com>
2021-10-19 18:01:06 -0700

    Initial commit

---
+++ app/src/androidTest/java/ParseApplication.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+
+import android.app.Application;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+//        Parse.initialize(new Parse.Configuration.Builder(this)
+//                .applicationId("U4lKz8zfiukpo7l8rHOIzRwTharn9DVTa6XPuAW0")
+//                .clientKey("4RcCqGk5YLDNCXgmeJiUZKsKSYfMPoF6591u9mMt")
+//                .server("https://parseapi.back4app.com")
+//                .build();
+    }
+}
